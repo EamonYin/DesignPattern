bb0e5cad3163943fb827f8ff86078a82151d4f05
XiaoMing <xiaominglovejava@163.com>
2020-12-16 20:55:00 +0800

    反射+抽象工厂模式（添加注释）

--- src/AbstractFactory/ReflecFactory/SuperFactory.java
+++ src/AbstractFactory/ReflecFactory/SuperFactory.java
@@ -17,18 +17,27 @@ public class SuperFactory {
     public void setClassName(String name) {
         this.className = name;
     }
-
+    //超级工厂中 生产耳机的 耳机工厂
     public static EarPhone CreatEarPhone() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         String classPosition = pack + "." + className;
         Class<?> aClass = Class.forName(classPosition);
         Object o = aClass.newInstance();
         return (EarPhone) o;
     }
-
+    //超级工厂中 生产手机的 手机工厂
     public static Phone CreatPhone() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         String classPosition = pack + "." + className;
         Class<?> aClass = Class.forName(classPosition);
         Object o = aClass.newInstance();
         return (Phone) o;
     }
+
+    /**
+     * 添加
+     * 超级工厂中 生产充电器的 充电器工厂
+     * 第一步：新建 充电器接口
+     * 第二步：新建 各品牌充电器实体类
+     * 第三步：在超级工厂类中添加 CreatCharger方法（反射）
+     */
+
 }
